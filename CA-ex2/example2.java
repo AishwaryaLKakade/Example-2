c3eebaf6c188631bd40b3acb4fcd30e0597638be
aishwarya kakade <aishwaryalokesh@hotmail.com>
2019-09-21 18:11:15 +0530

    example2

---
+++ CA-ex2/example2.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+
+class  example2
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Provide the number from 1 to 10");
+		int n = Integer.paresInt(args[0]);
+
+		
+
+		
+		
+
+
+
+
+		
+	}
+}
